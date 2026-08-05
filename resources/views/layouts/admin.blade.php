34a1ba6ff213c02ecfe82c2ef77d6f8cda32c278
Công Tử Hào Hoa <57801245+CongTuHaoHoa@users.noreply.github.com>
2026-08-05 21:08:56 +0700

    Fix lỗi route

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -25,7 +25,7 @@
 <body class="m-0 font-sans text-base antialiased font-normal dark:bg-slate-900 leading-default bg-gray-50 text-slate-500">
     <div class="absolute w-full bg-blue-500 dark:hidden min-h-75"></div>
     <!-- sidenav  -->
-    <aside class="fixed inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
+    <aside class="fixed h-full inset-y-0 flex-wrap items-center justify-between block w-full p-0 my-4 overflow-y-auto antialiased transition-transform duration-200 -translate-x-full bg-white border-0 shadow-xl dark:shadow-none dark:bg-slate-850 max-w-64 ease-nav-brand z-990 xl:ml-6 rounded-2xl xl:left-0 xl:translate-x-0" aria-expanded="false">
         <div class="h-19">
             <i class="absolute top-0 right-0 p-4 opacity-50 cursor-pointer fas fa-times dark:text-white text-slate-400 xl:hidden" sidenav-close></i>
             <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap dark:text-white text-slate-700" href="https://demos.creative-tim.com/argon-dashboard-tailwind/pages/dashboard.html" target="_blank">
@@ -74,12 +74,12 @@
                         <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
                             <i class="relative top-0 text-sm leading-normal text-red-600 fa-solid fa-people-group"></i>
                         </div>
-                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Nhân viên</span>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Tài khoản</span>
                     </a>
                 </li>
             </ul>
         </div>
-        <div class="mx-4">
+        <div class="mx-4 ">
             <!-- load phantom colors for card after: -->
             <p class="invisible hidden text-gray-800 text-red-500 text-red-600 text-blue-500 bg-gray-500/30 bg-cyan-500/30 bg-emerald-500/30 bg-orange-500/30 bg-red-500/30 after:bg-gradient-to-tl after:from-zinc-800 after:to-zinc-700 dark:bg-gradient-to-tl dark:from-slate-750 dark:to-gray-850 after:from-blue-700 after:to-cyan-500 after:from-orange-500 after:to-yellow-500 after:from-green-600 after:to-lime-400 after:from-red-600 after:to-orange-600 after:from-slate-600 after:to-slate-300 text-emerald-500 text-cyan-500 text-slate-400"></p>
             <div class="relative flex flex-col min-w-0 break-words bg-transparent border-0 shadow-none rounded-2xl bg-clip-border" sidenav-card>
@@ -90,8 +90,7 @@
                     </div>
                 </div>
             </div>
-            <a href="{{ route('admin.profile') }}" class="inline-block w-full px-8 py-2 mb-2 text-xs font-bold leading-normal text-center text-white capitalize transition-all ease-in rounded-lg shadow-md bg-blue-500 bg-150 hover:shadow-xs hover:-translate-y-px">Thông tin cá nhân</a>
-            <a class="inline-block w-full px-8 py-2 text-xs font-bold leading-normal text-center  text-danger border-danger border-2 align-middle transition-all ease-in rounded-lg shadow-md select-none bg-150 bg-x-25 hover:shadow-xs hover:-translate-y-px" href="https://www.creative-tim.com/product/argon-dashboard-pro-tailwind?ref=sidebarfree" target="_blank">Đăng xuất</a>
+            <a href="{{ route('client.index') }}" class="inline-block w-full px-8 py-2 mb-2 text-xs font-bold leading-normal text-center text-white capitalize transition-all ease-in rounded-lg shadow-md bg-blue-500 bg-150 hover:shadow-xs hover:-translate-y-px">Về trang chính</a>
         </div>
     </aside>
 
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('admin')->group(function () {
+Route::middleware('admin')->group(function ()
+{
     Route::get('/admin', 'App\Http\Controllers\Admin\AdminController@index')->name("admin.index");
 
     Route::get('/admin/food', 'App\Http\Controllers\Admin\FoodController@index')->name("food.index");
@@ -11,8 +12,8 @@ Route::middleware('admin')->group(function () {
     Route::delete('/admin/food/{id}', 'App\Http\Controllers\Admin\FoodController@delete')->name("food.delete");
     Route::put('/admin/food/{id}', 'App\Http\Controllers\Admin\FoodController@edit')->name("food.edit");
 
-    Route::get('/admin/food/{id}', 'App\Http\Controllers\Admin\FoodController@info')->name("food.info");
     Route::get('/admin/food/new', 'App\Http\Controllers\Admin\FoodController@new')->name("food.new");
+    Route::get('/admin/food/{id}', 'App\Http\Controllers\Admin\FoodController@info')->name("food.info");
 
     Route::get('/admin/category', 'App\Http\Controllers\Admin\CategoryController@index')->name("category.index");
     Route::get('/admin/category/new', 'App\Http\Controllers\Admin\CategoryController@new')->name("category.new");
