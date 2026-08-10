ad1a23c8d0c5a70caba699e1d04aa3a9e67ea4d7
Công Tử Hào Hoa <57801245+CongTuHaoHoa@users.noreply.github.com>
2026-08-10 17:54:49 +0700

    Đột phá tu vi tầng cuối cùng trước khi hắc hoá

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
+use App\Models\Order;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 
@@ -11,58 +12,9 @@ class AdminController extends Controller
         $viewData = [];
         $viewData["title"] = "Trang quản trị | Trang chủ";
         $viewData["subtitle"] = "Trang chủ";
+        $viewData["orders"] = Order::where('status', 1)->orderBy('created_at', 'asc')->limit(5)->get();
 
         $viewData["activate"] = "home";
         return view('admin.dashboard.index') -> with("viewData", $viewData);
-    }
-
-//    public function food(): Factory|View
-//    {
-//        $viewData = [];
-//        $viewData["title"] = "Trang quản trị | Món ăn";
-//        $viewData["subtitle"] = "Món ăn";
-//
-//        $viewData["activate"] = "food";
-//        return view('admin.food') -> with("viewData", $viewData);
-//    }
-
-    public function order(): Factory|View
-    {
-        $viewData = [];
-        $viewData["title"] = "Trang quản trị | Đơn hàng";
-        $viewData["subtitle"] = "Đơn hàng";
-
-        $viewData["activate"] = "order";
-        return view('admin.order.index') -> with("viewData", $viewData);
-    }
-
-    public function category(): Factory|View
-    {
-        $viewData = [];
-        $viewData["title"] = "Trang quản trị | Danh mục";
-        $viewData["subtitle"] = "Danh mục";
-
-        $viewData["activate"] = "category";
-        return view('admin.category.index') -> with("viewData", $viewData);
-    }
-
-    public function employee(): Factory|View
-    {
-        $viewData = [];
-        $viewData["title"] = "Trang quản trị | Nhân viên";
-        $viewData["subtitle"] = "Nhân viên";
-
-        $viewData["activate"] = "employee";
-        return view('admin.employee.index') -> with("viewData", $viewData);
-    }
-
-    public function profile(): Factory|View
-    {
-        $viewData = [];
-        $viewData["title"] = "Trang quản trị | Thông tin cá nhân";
-        $viewData["subtitle"] = "Thông tin cá nhân";
-
-        $viewData["activate"] = "profile";
-        return view('admin.profile.index') -> with("viewData", $viewData);
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,3 +1,114 @@
 @extends('layouts.admin')
 @section('title', $viewData['title'])
 @section('subtitle', $viewData['subtitle'])
+@section('content')
+    <div class="w-full px-6 py-6 mx-auto">
+        <!-- row 1 -->
+        <div class="flex flex-wrap -mx-3">
+            <!-- card1 -->
+            <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/2">
+                <div class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                    <div class="flex-auto p-4">
+                        <div class="flex flex-row -mx-3">
+                            <div class="flex-none w-2/3 max-w-full px-3">
+                                <div>
+                                    <p class="mb-0 font-sans text-sm font-semibold leading-normal uppercase dark:text-white dark:opacity-60">Doanh thu</p>
+                                    <h5 class="mb-2 font-bold dark:text-white">$53,000</h5>
+                                    <p class="mb-0 dark:text-white dark:opacity-60">
+                                        <span class="text-sm font-bold leading-normal text-emerald-500">+55%</span>
+                                        since yesterday
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="px-3 text-right basis-1/3">
+                                <div class="inline-block text-white w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-blue-500 to-violet-500">
+                                    <div class="flex align-items-center justify-content-center w-100 h-100">
+                                        <i class="fa-solid fa-money-bill"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- card2 -->
+            <div class="w-full max-w-full px-3 mb-6 sm:w-1/2 sm:flex-none xl:mb-0 xl:w-1/2">
+                <div class="relative flex flex-col min-w-0 break-words bg-white shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
+                    <div class="flex-auto p-4">
+                        <div class="flex flex-row -mx-3">
+                            <div class="flex-none w-2/3 max-w-full px-3">
+                                <div>
+                                    <p class="mb-0 font-sans text-sm font-semibold leading-normal uppercase dark:text-white dark:opacity-60">Khách hàng</p>
+                                    <h5 class="mb-2 font-bold dark:text-white">2,300</h5>
+                                    <p class="mb-0 dark:text-white dark:opacity-60">
+                                        <span class="text-sm font-bold leading-normal text-emerald-500">+3%</span>
+                                        since last week
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="px-3 text-right basis-1/3">
+                                <div class="inline-block text-white w-12 h-12 text-center rounded-circle bg-gradient-to-tl from-red-600 to-orange-600">
+                                    <div class="flex align-items-center justify-content-center w-100 h-100">
+                                        <i class="fa-solid fa-users"></i>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- cards row 2 -->
+        <div class="flex flex-wrap mt-6 -mx-3">
+            <div class="w-full max-w-full px-3 mt-0 lg:w-7/12 lg:flex-none">
+                <div class="border-black/12.5 dark:bg-slate-850 dark:shadow-dark-xl shadow-xl relative z-20 flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border">
+                    <div class="border-black/12.5 mb-0 rounded-t-2xl border-b-0 border-solid p-6 pt-4 pb-0">
+                        <h6 class="capitalize dark:text-white">Doanh thu</h6>
+                        <p class="mb-0 text-sm leading-normal dark:text-white dark:opacity-60">
+                            <i class="fa fa-arrow-up text-emerald-500"></i>
+                            <span class="font-semibold">4% more</span> in 2021
+                        </p>
+                    </div>
+                    <div class="flex-auto p-4">
+                        <div>
+                            <canvas id="chart-line" height="300"></canvas>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="w-full max-w-full px-3 mt-0 lg:w-5/12 lg:flex-none">
+                <div class="border-black/12.5 shadow-xl dark:bg-slate-850 dark:shadow-dark-xl relative flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border">
+                    <div class="p-4 pb-0 rounded-t-4">
+                        <h6 class="mb-0 dark:text-white">Đơn hàng đang đợi</h6>
+                    </div>
+                    <div class="flex-auto p-4">
+                        <ul class="flex flex-col pl-0 mb-0 rounded-lg">
+                            @foreach($viewData['orders'] as $order)
+                                <li class="relative flex justify-between py-2 pr-4 mb-2 border-0 rounded-t-lg rounded-xl text-inherit">
+                                    <div class="flex items-center">
+                                        <img src="{{ asset('/storage/user/'.$order->user->avatar) }}" class="inline-flex items-center justify-center mr-4 text-sm text-white transition-all duration-200 ease-in-out h-9 w-9 rounded-xl" alt="user1" />
+                                        <div class="flex flex-col">
+                                            <h6 class="mb-1 text-sm leading-normal text-slate-700 dark:text-white">{{ $order->user->name }}</h6>
+                                            <span class="text-xs leading-tight dark:text-white/80">{{ $order->foodOrders->sum('quantity') }} món</span>
+                                        </div>
+                                    </div>
+                                    <div class="flex">
+                                        <a href="{{ route('order.info', $order->id) }}" class="group ease-in leading-pro text-xs rounded-3.5xl p-1.2 h-6.5 w-6.5 mx-0 my-auto inline-block cursor-pointer border-0 bg-transparent text-center align-middle font-bold text-slate-700 shadow-none transition-all dark:text-white">
+                                            <i class="fa-solid fa-arrow-right-long"></i>
+                                        </a>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+    </div>
+@endsection
