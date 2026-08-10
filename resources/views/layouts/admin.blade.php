e8b52781d8fe481136a391cee567d86c1371a816
Công Tử Hào Hoa <57801245+CongTuHaoHoa@users.noreply.github.com>
2026-08-10 21:31:07 +0700

    Fix linh tinh

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -46,19 +46,19 @@
                     </a>
                 </li>
                 <li class="mt-0.5 w-full">
-                    <a class="py-2.7 {{ $viewData['activate'] == 'food' ? 'bg-blue-500/13 font-semibold text-slate-700' : '' }} dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 transition-colors" href="{{ route('food.index')  }}">
-                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
-                            <i class="relative top-0 text-sm leading-normal text-orange-500 fa-solid fa-utensils"></i>
-                        </div>
-                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Món ăn</span>
-                    </a>
-                </li>
-                <li class="mt-0.5 w-full">
                     <a class="py-2.7 {{ $viewData['activate'] == 'order' ? 'bg-blue-500/13 font-semibold text-slate-700' : '' }} dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 transition-colors" href="{{ route('order.index') }}">
                         <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center fill-current stroke-0 text-center xl:p-2.5">
                             <i class="relative top-0 text-sm leading-normal text-emerald-500 fa-solid fa-bag-shopping"></i>
                         </div>
                         <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Đơn hàng</span>
+                    </a>
+                </li>
+                <li class="mt-0.5 w-full">
+                    <a class="py-2.7 {{ $viewData['activate'] == 'food' ? 'bg-blue-500/13 font-semibold text-slate-700' : '' }} dark:text-white dark:opacity-80 text-sm ease-nav-brand my-0 mx-2 flex items-center whitespace-nowrap rounded-lg px-4 transition-colors" href="{{ route('food.index')  }}">
+                        <div class="mr-2 flex h-8 w-8 items-center justify-center rounded-lg bg-center stroke-0 text-center xl:p-2.5">
+                            <i class="relative top-0 text-sm leading-normal text-orange-500 fa-solid fa-utensils"></i>
+                        </div>
+                        <span class="ml-1 duration-300 opacity-100 pointer-events-none ease">Món ăn</span>
                     </a>
                 </li>
                 <li class="mt-0.5 w-full">
