48c0464e0e4f5445711053cea934f3ee451e2d38
Công Tử Hào Hoa <57801245+CongTuHaoHoa@users.noreply.github.com>
2026-08-10 21:21:21 +0700

    Fix linh tinh

--- resources/views/admin/order/index.blade.php
+++ resources/views/admin/order/index.blade.php
@@ -6,7 +6,7 @@
         <div class="flex-none w-full max-w-full px-3">
             <div class="relative flex flex-col min-w-0 mb-6 wrap-break-word bg-white border-0 border-transparent border-solid shadow-xl dark:bg-slate-850 dark:shadow-dark-xl rounded-2xl bg-clip-border">
                 <div class="p-6 pb-0 mb-0 border-b-0 border-b-solid rounded-t-2xl border-b-transparent flex justify-center">
-                    <h6 class="dark:text-white grow">Đơn </h6>
+                    <h6 class="dark:text-white grow">Đơn hàng</h6>
                 </div>
                 <div class="flex-auto px-0 pt-0 pb-2 grow">
                     <div class="p-0 overflow-x-auto">
